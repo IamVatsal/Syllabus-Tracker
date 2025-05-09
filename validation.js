ebcaa2e0033b7e891cb5fc6a693dd6351ee10edc
IamVatsal <vatsalpatel0609@gmail.com>
2025-05-09 15:37:59 +0530

    add phonenumber validation

--- validation.js
+++ validation.js
@@ -4,13 +4,14 @@ const email_input = document.getElementById('email-input')
 const password_input = document.getElementById('password-input')
 const repeat_password_input = document.getElementById('repeat-password-input')
 const error_message = document.getElementById('error-message')
+const phone_input = document.getElementById('phone-input')
 
 form.addEventListener('submit', (e) => {
   let errors = []
-
   if(firstname_input){
     // If we have a firstname input then we are in the signup
-    errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
+    errors = getSignupFormErrors(firstname_input.value, email_input.value, phone_input.value, password_input.value, repeat_password_input.value)
+  
   }
   else{
     // If we don't have a firstname input then we are in the login
@@ -24,7 +25,7 @@ form.addEventListener('submit', (e) => {
   }
 })
 
-function getSignupFormErrors(firstname, email, password, repeatPassword){
+function getSignupFormErrors(firstname, email, phoneNumber, password, repeatPassword,){
   let errors = []
 
   if(firstname === '' || firstname == null){
@@ -48,7 +49,25 @@ function getSignupFormErrors(firstname, email, password, repeatPassword){
     password_input.parentElement.classList.add('incorrect')
     repeat_password_input.parentElement.classList.add('incorrect')
   }
-
+  if(phoneNumber === '' || phoneNumber == null){
+    errors.push('Phone number is required')
+    phone_input.parentElement.classList.add('incorrect')
+  }
+  if(phoneNumber.length < 10){
+    errors.push('Phone number must have at least 10 digits')
+    phone_input.parentElement.classList.add('incorrect')
+  }
+  if(phoneNumber.length > 12){
+    errors.push('Phone number must have at most 12 digits')
+    phone_input.parentElement.classList.add('incorrect')
+  }
+  if(!(String(phoneNumber)[0] == 6 ||
+     String(phoneNumber)[0] == 7 ||
+     String(phoneNumber)[0] == 8 ||
+     String(phoneNumber)[0] == 9)){
+    errors.push('Phone number must start with 6, 7, 8 or 9')
+    phone_input.parentElement.classList.add('incorrect')
+  }
 
   return errors;
 }
@@ -68,7 +87,7 @@ function getLoginFormErrors(email, password){
   return errors;
 }
 
-const allInputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
+const allInputs = [firstname_input, email_input, phone_input, password_input, repeat_password_input].filter(input => input != null)
 
 allInputs.forEach(input => {
   input.addEventListener('input', () => {
